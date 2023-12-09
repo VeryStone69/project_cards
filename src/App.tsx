8a3c31c74ab7ac379939b52faba15c6796a9be6e
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-12-09 19:23:23 +0100

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button>Primary Button</Button>
+      <Button<'a'> href={'google.com'} rel={'noopener noreferrer'} target={'_blank'}>
+        Primary Button 2
+      </Button>
+    </div>
+  )
 }
