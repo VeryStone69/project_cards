abe88b8a9e26971a3d64885ab084e5a1a61d91f3
atryomkorshykau <butovichjunior@gmail.com>
2023-12-19 16:12:56 +0300

    add Card story with form

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -2,10 +2,11 @@ import {Meta, StoryObj} from '@storybook/react'
 
 import {Card} from './'
 import {Typography} from "@/components/ui/typography";
+import {SignIn} from "@/components/forms/sign-in";
 
 const meta = {
     component: Card,
-    title: 'Components/Forms/Card',
+    title: 'Components/ui/Card',
 } satisfies Meta<typeof Card>
 
 export default meta
@@ -14,6 +15,19 @@ type Story = StoryObj<typeof meta>
 export const DefaultCard: Story = {
     args: {
         children: <Typography variant={'large'}></Typography>,
+        style: {
+            height: '520px',
+            padding: '24px',
+            width: '420px',
+        },
+    },
+}
+
+export const LoginCard: Story = {
+    args: {
+        children:
+            (<SignIn onSubmit={() => {
+            }}></SignIn>),
         style: {
             height: '520px',
             padding: '24px',
