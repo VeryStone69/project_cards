1412fd2df896f6ab56bbcea9cf7aa58f71c4f5d2
Alexander Korobov <dktwho@mail.ru>
2023-12-20 20:44:34 +0300

    feat: modified dropdown

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -1,54 +1,47 @@
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-// import {
-//   HamburgerMenuIcon,
-//   DotFilledIcon,
-//   CheckIcon,
-//   ChevronRightIcon,
 import { Icon } from '@/components/icon/Icon'
-// } from '@radix-ui/react-icons';
 import { Typography } from '@/components/ui/typography'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import s from './dropdown.module.scss'
 
 const DropdownMenuDemo = () => {
-  // const [bookmarksChecked, setBookmarksChecked] = React.useState(true)
-  // const [urlsChecked, setUrlsChecked] = React.useState(false)
-  // const [person, setPerson] = React.useState('pedro')
-
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <img className={s['userLogo']} />
-      </DropdownMenu.Trigger>
+    <div className={s['DropdownMenuRoot']}>
+      <DropdownMenu.Root open>
+        <DropdownMenu.Trigger asChild>
+          <img className={s['userLogo']} />
+        </DropdownMenu.Trigger>
 
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={5}>
-          <DropdownMenu.Arrow className={s['DropdownMenuArrow']}>
-            <Icon name={'beak-container-top'} />
-          </DropdownMenu.Arrow>
-          <DropdownMenu.Item className={s['DropdownMenuItem']}>
-            <img className={s['userLogo']} />
-            <div className={s['userDataName']}>
-              <Typography variant={'subtitle1'}>Ivan</Typography>
-            </div>
-            <div className={s['userDataEmail']}>
-              <Typography variant={'caption'}>j&johnson@gmail.com</Typography>
-            </div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Separator className={s['DropdownMenuSeparator']} />
-          <DropdownMenu.Item className={s['DropdownMenuItem']}>
-            <Icon name={'logoUserDrop'} />
-            My Profile
-          </DropdownMenu.Item>
-          <DropdownMenu.Separator className={s['DropdownMenuSeparator']} />
-          <DropdownMenu.Item className={s['DropdownMenuItem']}>
-            <Icon name={'logout'} />
-            Sign Out
-          </DropdownMenu.Item>
+        <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>
+          <DropdownMenu.Arrow className={s['DropdownMenuArrow']} height={8} width={14} />
+          <DropDownItem />
+          <DropDownItemWithIcon />
+          <DropDownItemWithIcon />
         </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+      </DropdownMenu.Root>
+    </div>
   )
 }
 
 export default DropdownMenuDemo
+
+export const DropDownItem = () => {
+  return (
+    <DropdownMenu.Item className={s['DropdownMenuItem']}>
+      <img className={s['userLogo']} />
+      <div>
+        <Typography variant={'subtitle1'}>Ivan</Typography>
+        <Typography variant={'caption'}>j&johnson@gmail.com</Typography>
+      </div>
+    </DropdownMenu.Item>
+  )
+}
+
+export const DropDownItemWithIcon = () => {
+  return (
+    <div className={s['item-with-icon']}>
+      <Icon className={s['svg-white']} name={'logoUserDrop'} size={'16px'} />
+      <Typography variant={'caption'}>Ivan</Typography>
+    </div>
+  )
+}
