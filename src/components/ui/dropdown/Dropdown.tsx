1849525704644fbdeefe596f521e89fdbaad0b9c
Alexander Korobov <dktwho@mail.ru>
2023-12-21 16:16:53 +0300

    feat: fixed sprite icons, prepare onSelect.

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -1,5 +1,3 @@
-import { ReactNode } from 'react'
-
 import { Icon } from '@/components/icon/Icon'
 import { Typography } from '@/components/ui/typography'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
@@ -10,35 +8,37 @@ const DropdownMenuDemo = () => {
   return (
     <div className={s['DropdownMenuRoot']}>
       <DropdownMenu.Root>
-        {/*<DropdownMenu.Trigger asChild>*/}
-        {/*  <img className={s['userLogo']} />*/}
-        {/*</DropdownMenu.Trigger>*/}
-
-        {/*<DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>*/}
-        {/*  <DropdownMenu.Arrow className={s['DropdownMenuArrow']} height={8} width={14} />*/}
-
-        {/*  <DropDownItem*/}
-        {/*    classNameItem={s.DropdownMenuItem}*/}
-        {/*    classNameSVG={s.userLogo}*/}
-        {/*    userEmail={'j&johnson@gmail.com'}*/}
-        {/*    userName={'Ivan'}*/}
-        {/*  />*/}
-        {/*  <DropDownItemWithIcon*/}
-        {/*    classNameItem={s.itemWithIcon}*/}
-        {/*    classNameSVG={s.svgWhite}*/}
-        {/*    svgId={'logoUserDrop'}*/}
-        {/*    textItem={'My profile'}*/}
-        {/*  />*/}
-        {/*  <DropDownItemWithIcon*/}
-        {/*    classNameItem={s.itemWithIcon}*/}
-        {/*    classNameSVG={s.svgWhite}*/}
-        {/*    svgId={'logout'}*/}
-        {/*    textItem={'Sign Out'}*/}
-        {/*  />*/}
-        {/*</DropdownMenu.Content>*/}
-
         <DropdownMenu.Trigger asChild>
           <img className={s['userLogo']} />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>
+          <DropdownMenu.Arrow className={s['DropdownMenuArrow']} height={8} width={14} />
+
+          <DropDownItem
+            classNameItem={s.DropdownMenuItem}
+            classNameSVG={s.userLogo}
+            userEmail={'j&johnson@gmail.com'}
+            userName={'Ivan'}
+          />
+          <DropDownItemWithIcon
+            classNameItem={s.itemWithIcon}
+            classNameSVG={s.svgWhite}
+            svgId={'logoUserDrop'}
+            textItem={'My profile'}
+          />
+          <DropDownItemWithIcon
+            classNameItem={s.itemWithIcon}
+            classNameSVG={s.svgWhite}
+            svgId={'logout'}
+            textItem={'Sign Out'}
+          />
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <button>test</button>
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>
@@ -67,30 +67,26 @@ const DropdownMenuDemo = () => {
   )
 }
 
-// collapsed?: boolean
-// icon?: ReactNode
-// svgId?: string
-// textItem?: string
-
 export type DropDownProps = {
   classNameItem?: string
   classNameSVG?: string
+  onSelect?: (event: Event) => void
   userEmail?: string
   userName?: string
 }
 
-type DropDownWithIconProps = Pick<DropDownProps, 'classNameItem' | 'classNameSVG'> & {
-  svgId?: string
+type DropDownWithIconProps = Pick<DropDownProps, 'classNameItem' | 'classNameSVG' | 'onSelect'> & {
+  svgId: string
   textItem?: string
 }
 
 export default DropdownMenuDemo
 
 export const DropDownItem = (props: DropDownProps) => {
-  const { classNameItem, classNameSVG, userEmail, userName } = props
+  const { classNameItem, classNameSVG, onSelect, userEmail, userName } = props
 
   return (
-    <DropdownMenu.Item className={classNameItem}>
+    <DropdownMenu.Item className={classNameItem} onSelect={onSelect}>
       <img className={classNameSVG} />
       <div>
         <Typography variant={'subtitle1'}>{userName}</Typography>
@@ -101,10 +97,10 @@ export const DropDownItem = (props: DropDownProps) => {
 }
 
 export const DropDownItemWithIcon = (props: DropDownWithIconProps) => {
-  const { classNameItem, classNameSVG, svgId, textItem } = props
+  const { classNameItem, classNameSVG, onSelect, svgId, textItem } = props
 
   return (
-    <DropdownMenu.Item className={classNameItem}>
+    <DropdownMenu.Item className={classNameItem} onSelect={onSelect}>
       <Icon className={classNameSVG} name={svgId} size={'16px'} />
       <Typography variant={'caption'}>{textItem}</Typography>
     </DropdownMenu.Item>
--- src/components/ui/dropdown/dropdown.stories.tsx
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -1,5 +1,11 @@
-import DropdownMenuDemo from '@/components/ui/dropdown/Dropdown'
+import DropdownMenuDemo, {
+  DropDownItem,
+  DropDownItemWithIcon,
+} from '@/components/ui/dropdown/Dropdown'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { Meta, StoryObj } from '@storybook/react'
+
+import s from '@/components/ui/dropdown/dropdown.module.scss'
 
 const meta = {
   argTypes: {},
@@ -12,14 +18,85 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const DropDown: Story = {
+export const DropDownWithlUserLogo: Story = {
   args: {
     label: 'This is dropdown',
   },
-  render: function Render(args) {
+  render: function Render() {
     return (
       <>
-        <DropdownMenuDemo {...args} />
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <img className={s['userLogo']} />
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>
+            <DropdownMenu.Arrow className={s['DropdownMenuArrow']} height={8} width={14} />
+
+            <DropDownItem
+              classNameItem={s.DropdownMenuItem}
+              classNameSVG={s.userLogo}
+              userEmail={'j&johnson@gmail.com'}
+              userName={'Ivan'}
+            />
+            <DropDownItemWithIcon
+              classNameItem={s.itemWithIcon}
+              classNameSVG={s.svgWhite}
+              svgId={'logoUserDrop'}
+              textItem={'My profile'}
+            />
+            <DropDownItemWithIcon
+              classNameItem={s.itemWithIcon}
+              classNameSVG={s.svgWhite}
+              svgId={'logout'}
+              textItem={'Sign Out'}
+            />
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </>
+    )
+  },
+}
+
+export const DropDownWithSVGList: Story = {
+  args: {
+    label: 'This is dropdown',
+  },
+  render: function Render() {
+    return (
+      <>
+        <span> This is default drop-down-menu button:</span>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button
+              style={{ border: '1px solid yellowgreen', borderRadius: '5px', margin: '10px' }}
+            >
+              test
+            </button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Content className={s['DropdownMenuContent']} sideOffset={8}>
+            <DropdownMenu.Arrow className={s['DropdownMenuArrow']} height={8} width={14} />
+            <DropDownItemWithIcon
+              classNameItem={s.itemWithIcon}
+              classNameSVG={s.svgWhite}
+              svgId={'playLearn'}
+              textItem={'My profile'}
+            />
+            <DropDownItemWithIcon
+              classNameItem={s.itemWithIcon}
+              classNameSVG={s.svgWhite}
+              svgId={'edit'}
+              textItem={'My profile'}
+            />
+            <DropDownItemWithIcon
+              classNameItem={s.itemWithIcon}
+              classNameSVG={s.svgWhite}
+              svgId={'trashBin'}
+              textItem={'Sign Out'}
+            />
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </>
     )
   },
