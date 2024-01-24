5d2116a6e4389a1b577150e1e3a2645a254afccb
atryomkorshykau <butovichjunior@gmail.com>
2024-01-24 20:08:55 +0300

    add: ui/file-uploader

---
+++ src/components/ui/loader/loader.stories.tsx
@@ -0,0 +1,20 @@
+import type {Meta, StoryObj} from '@storybook/react'
+
+import {InitialLoader} from "@/components/ui/loader/loader";
+
+const meta = {
+    component: InitialLoader,
+    parameters: {
+        layout: 'centered',
+    },
+    title: 'Components/ui/Loader',
+} satisfies Meta<typeof InitialLoader>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Loader: Story = {
+    args: {
+        preLoader: true
+    },
+}
