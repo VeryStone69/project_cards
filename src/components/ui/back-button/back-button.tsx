972a2173f259ff30205244a0a452b526f3283027
Alexander Khomenok <exleonardo@mail.ru>
2024-01-25 00:15:07 +0300

    feat(component): add back-button.tsx

---
+++ src/components/ui/back-button/back-button.tsx
@@ -0,0 +1,38 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+import { Icon } from '@/components/icon/Icon'
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+import { clsx } from 'clsx'
+
+import s from './back-button.module.scss'
+
+type Props = {
+  className?: string
+  text?: string
+}
+
+export const BackButton = ({ className, text = 'Back to Previous Page', ...props }: Props) => {
+  const navigate = useNavigate()
+  const classes = clsx(s.button, className)
+  const backHandler = () => {
+    navigate(-1)
+  }
+
+  return (
+    <Button
+      as={Link}
+      className={classes}
+      onClick={backHandler}
+      relative={'path'}
+      to={'..'}
+      variant={'link'}
+      {...props}
+    >
+      <Icon name={'arrow-back'} />
+      <Typography className={s.text} variant={'body2'}>
+        {text}
+      </Typography>
+    </Button>
+  )
+}
