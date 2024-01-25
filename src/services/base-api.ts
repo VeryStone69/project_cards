d86121744db9e447768ec471fb058f14152fdd77
Alexander Khomenok <exleonardo@mail.ru>
2024-01-25 15:32:01 +0300

    refactor(api): create new endpoints

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,4 +1,3 @@
-import { GetDecks, GetDecksArgs } from '@/services/base-api.types'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
@@ -9,20 +8,7 @@ export const baseApi = createApi({
       headers.append('x-auth-skip', 'true')
     },
   }),
-  endpoints: builder => {
-    return {
-      getDecks: builder.query<GetDecks, Partial<GetDecksArgs> | void>({
-        query: arg => {
-          return {
-            method: 'GET',
-            params: arg ?? {},
-            url: `v1/decks`,
-          }
-        },
-      }),
-    }
-  },
+  endpoints: () => ({}),
   reducerPath: 'baseApi',
+  tagTypes: ['Decks', 'Deck'],
 })
-
-export const { useGetDecksQuery, useLazyGetDecksQuery } = baseApi
