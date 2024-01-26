1eb2dc2b92dccf8c4a0871733e6fc755bed9d0c6
Alexander Khomenok <exleonardo@mail.ru>
2024-01-26 23:28:28 +0300

    feat: add cards-api.ts

---
+++ src/services/cards-api.ts
@@ -0,0 +1,71 @@
+import { baseApi } from '@/services/base-api'
+import { CardRate, CardResponse, CardsItem } from '@/services/cards-api-types'
+
+export const cardsApi = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      createCardsInDeck: builder.mutation<Omit<CardsItem, 'grade'>, { data: FormData; id: string }>(
+        {
+          invalidatesTags: ['Cards'],
+          query: arg => {
+            return {
+              body: arg.data,
+              method: 'POST',
+              url: `v1/decks/${arg.id}/cards`,
+            }
+          },
+        }
+      ),
+      deleteCard: builder.mutation<void, { id: string }>({
+        invalidatesTags: ['Cards'],
+        query: arg => ({
+          body: arg,
+          method: 'DELETE',
+          url: `v1/cards/${arg.id}`,
+        }),
+      }),
+      getCardsInDeck: builder.query<CardResponse, { id: string }>({
+        query: arg => {
+          return {
+            body: arg.id,
+            method: 'GET',
+            url: `v1/decks/${arg.id}/cards`,
+          }
+        },
+      }),
+      getLearnCard: builder.query<CardsItem, { id: string }>({
+        query: arg => ({
+          method: 'GET',
+          params: arg,
+          url: `v1/decks/${arg.id}/learn`,
+        }),
+      }),
+      updateCard: builder.mutation<Omit<CardsItem, 'grade'>, { cardId: string; data: FormData }>({
+        invalidatesTags: ['Cards'],
+        query: ({ cardId, data }) => ({
+          body: data,
+          method: 'PATCH',
+          url: `v1/cards/${cardId}`,
+        }),
+      }),
+      updateRateCard: builder.mutation<CardsItem, CardRate>({
+        query: ({ packId, ...rest }) => ({
+          method: 'POST',
+          params: rest,
+          url: `v1/decks/${packId}/learn`,
+        }),
+      }),
+    }
+  },
+})
+
+export const {
+  useCreateCardsInDeckMutation,
+  useDeleteCardMutation,
+  useGetCardsInDeckQuery,
+  useGetLearnCardQuery,
+  useLazyGetCardsInDeckQuery,
+  useLazyGetLearnCardQuery,
+  useUpdateCardMutation,
+  useUpdateRateCardMutation,
+} = cardsApi
