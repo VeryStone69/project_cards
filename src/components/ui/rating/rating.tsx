fa1f5f9b160a5fbd829e53316b2e61162b01cd93
Alexander Khomenok <exleonardo@mail.ru>
2024-01-27 16:09:57 +0300

    fix: rating component

--- src/components/ui/rating/rating.tsx
+++ src/components/ui/rating/rating.tsx
@@ -1,18 +1,27 @@
-import {Star} from "@/components/ui/rating/star/star";
+import { memo } from 'react'
+
+import { Icon } from '@/components/icon/Icon'
+
 import s from './rating.module.scss'
-import {useState} from "react";
 
-export const Rating = () => {
+type Props = {
+  className?: string
+  rating?: number
+  size?: string
+}
+export const Rating = memo(({ className, rating = 0, size = '16px' }: Props) => {
+  const starItem = [1, 2, 3, 4, 5]
 
-    const [value, setValue] = useState(0)
-    return (
-        <div className={s.rating}>
-            <Star selected={value > 0} setValue={setValue} value={1}/>
-            <Star selected={value > 1} setValue={setValue} value={2}/>
-            <Star selected={value > 2} setValue={setValue} value={3}/>
-            <Star selected={value > 3} setValue={setValue} value={4}/>
-            <Star selected={value > 4} setValue={setValue} value={5}/>
-        </div>
-    );
-};
-
+  return (
+    <div className={className}>
+      {starItem.map(star => (
+        <Icon
+          className={rating >= star ? s.starRate : s.starNoRate}
+          key={star}
+          name={'star'}
+          size={size}
+        />
+      ))}
+    </div>
+  )
+})
