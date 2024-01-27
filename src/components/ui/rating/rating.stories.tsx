76b141824abd1b82d9e1109746f7606cbe19b8fa
Alexander Khomenok <exleonardo@mail.ru>
2024-01-27 16:10:15 +0300

    feat: add stories

--- src/components/ui/rating/rating.stories.tsx
+++ src/components/ui/rating/rating.stories.tsx
@@ -1,23 +1,25 @@
-import type {Meta, StoryObj} from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-import {Rating} from "@/components/ui/rating/rating";
+import { Rating } from '@/components/ui/rating/rating'
 
 const meta = {
-    component: Rating,
-    parameters: {
-        layout: 'centered',
-    },
-    title: 'Components/ui/Rating',
+  component: Rating,
+  parameters: {
+    layout: 'centered',
+  },
+  title: 'Components/ui/Rating',
 } satisfies Meta<typeof Rating>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    render() {
-        return (
-            <Rating/>
-        )
-    },
+  render() {
+    return <Rating />
+  },
 }
-
+export const RatingValue: Story = {
+  render() {
+    return <Rating rating={4} />
+  },
+}
