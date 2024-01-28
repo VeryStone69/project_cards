11c57d4e0477a005dfe2416d421cc6bab8244792
atryomkorshykau <butovichjunior@gmail.com>
2024-01-28 17:18:51 +0300

    add: services/admin-api

--- src/services/admin/admin.ts
+++ src/services/admin/admin.ts
@@ -1,44 +1,39 @@
-import { baseApi } from '@/services/base-api'
 import { DeleteUser, NewUserData } from '@/services/admin/admin.types'
+import { baseApi } from '@/services/base-api'
 
 export const adminAPI = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
-      getAllUsers: builder.query({
-        query: () => `/v1/users`,
-      }),
       addUser: builder.mutation<NewUserData, {}>({
         query: args => {
           return {
             args,
-            url: `/v1/users`,
             method: 'POST',
+            url: `/v1/users`,
           }
         },
       }),
       deleteAllUsers: builder.mutation({
         query: () => {
           return {
-            url: `/v1/users`,
             method: 'DELETE',
+            url: `/v1/users`,
           }
         },
       }),
       deleteUser: builder.mutation<DeleteUser, {}>({
         query: (id: string) => {
           return {
-            url: `/v1/users/${id}`,
             method: 'DELETE',
+            url: `/v1/users/${id}`,
           }
         },
+      }),
+      getAllUsers: builder.query({
+        query: () => `/v1/users`,
       }),
     }
   },
 })
 
-export const {
-  useGetAllUsersQuery,
-  useAddUserMutation,
-  useDeleteAllUsersMutation,
-  useDeleteUserMutation,
-} = adminAPI
+export const {} = adminAPI
