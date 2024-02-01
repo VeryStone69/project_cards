cba6dd76a5b2a5531c6d87b3a3cbca34c280e698
Alexander Khomenok <exleonardo@mail.ru>
2024-02-01 15:46:57 +0300

    refactor: profile.tsx

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,5 +1,5 @@
-import { Header } from '@/components/header'
 import { Icon } from '@/components/icon/Icon'
+import { Layout } from '@/components/layout'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { FileUploader } from '@/components/ui/file-uploader'
@@ -11,29 +11,30 @@ import avatar from '../../assets/images/avatar.jpg'
 
 export const Profile = () => {
   return (
-    <div className={s.profilePage}>
-      <Header data={{ avatar: '', email: '', name: 'Artyom Korshykau' }} />
-      <Card className={s.card}>
-        <Typography className={s.title} variant={'h1'}>
-          Personal Information
-        </Typography>
-        <div className={s.avatarBlock}>
-          <img alt={'avatar'} className={s.avatar} src={avatar} />
-          <div className={s.fileUploader}>
-            <FileUploader />
+    <Layout>
+      <div className={s.profilePage}>
+        <Card className={s.card}>
+          <Typography className={s.title} variant={'h1'}>
+            Personal Information
+          </Typography>
+          <div className={s.avatarBlock}>
+            <img alt={'avatar'} className={s.avatar} src={avatar} />
+            <div className={s.fileUploader}>
+              <FileUploader />
+            </div>
           </div>
-        </div>
-        <div className={s.nameBlock}>
-          <Typography variant={'h2'}>Artyom</Typography>
-          <Icon fill={'white'} height={'17px'} name={'edit'} width={'17px'} />
-        </div>
-        <Typography className={s.email} variant={'body2'}>
-          example@gmail.com
-        </Typography>
-        <Typography variant={'subtitle2'}>
-          <Button variant={'secondary'}>Logout</Button>
-        </Typography>
-      </Card>
-    </div>
+          <div className={s.nameBlock}>
+            <Typography variant={'h2'}>Artyom</Typography>
+            <Icon fill={'white'} height={'17px'} name={'edit'} width={'17px'} />
+          </div>
+          <Typography className={s.email} variant={'body2'}>
+            example@gmail.com
+          </Typography>
+          <Typography variant={'subtitle2'}>
+            <Button variant={'secondary'}>Logout</Button>
+          </Typography>
+        </Card>
+      </div>
+    </Layout>
   )
 }
