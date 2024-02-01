0ca3fcbf6fa470f584d69b6aac73de771b0ef675
Alexander Khomenok <exleonardo@mail.ru>
2024-02-01 14:43:21 +0300

    feat: add dropdown to header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,11 @@
 import { memo } from 'react'
 
 import { Logo } from '@/assets/illustrations/logo'
+import { Avatar } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import { Dropdown } from '@/components/ui/dropdown'
 import { Typography } from '@/components/ui/typography'
+import { DropDownUser } from '@/features/dropdown-user/drop-down-user'
 
 import s from './header.module.scss'
 
@@ -29,7 +32,17 @@ export const Header = memo(({ data }: Props) => {
             <Typography className={s.userName} variant={'subtitle1'}>
               {data.name || data.email}
             </Typography>
-            <img alt={'avatar'} className={s.userPhoto} src={avatar} />
+            <Dropdown
+              align={'end'}
+              sideOffset={-6}
+              trigger={
+                <button>
+                  <Avatar className={s.userPhoto} src={avatar} userName={data.name} />
+                </button>
+              }
+            >
+              <DropDownUser userEmail={data.email} userName={data.name} />
+            </Dropdown>
           </div>
         ) : (
           <Button variant={'secondary'}>Sign in</Button>
