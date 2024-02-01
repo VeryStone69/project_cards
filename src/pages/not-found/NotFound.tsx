24b283497819a42be132c409dd74a88d06799614
atryomkorshykau <butovichjunior@gmail.com>
2024-02-01 15:18:40 +0300

    fix: pages/not-found

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,28 +1,21 @@
-import { Header } from '@/components/header'
+import { Layout } from '@/components/layout'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 
-import styles from './NotFound.module.scss'
+import s from './NotFound.module.scss'
 
 import notFound from '../../assets/images/not-found.png'
 
 export const NotFound = () => {
   return (
-    <div>
-      <Header
-        data={{
-          avatar: '',
-          email: 'junior.gmail.com',
-          name: 'Artyom Korshykau',
-        }}
-      />
-      <div className={styles.notfound}>
+    <Layout>
+      <div className={s.notfound}>
         <img alt={'page not found'} src={notFound} />
         <Typography variant={'body1'}>Page not found!</Typography>
         <Typography variant={'subtitle2'}>
-          <Button variant={'primary'}>Back to home page</Button>
+          <Button variant={'primary'}>Back</Button>
         </Typography>
       </div>
-    </div>
+    </Layout>
   )
 }
