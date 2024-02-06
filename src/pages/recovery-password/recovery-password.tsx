bcf3fc6a0945c304307fadcad328655d12b26471
Alexander Khomenok <exleonardo@mail.ru>
2024-02-06 23:55:41 +0300

    refactor: fix <Layout>

--- src/pages/recovery-password/recovery-password.tsx
+++ src/pages/recovery-password/recovery-password.tsx
@@ -1,20 +1,17 @@
 import { ForgotPassword } from '@/components/forms/forgot-password'
-import { Layout } from '@/components/layout'
 import { Card } from '@/components/ui/card'
 
 import styles from './recovery-password.module.scss'
 
 const RecoveryPassword = () => {
   return (
-    <Layout>
-      <Card className={styles.card}>
-        <ForgotPassword
-          onSubmit={values => {
-            alert(JSON.stringify(values))
-          }}
-        />
-      </Card>
-    </Layout>
+    <Card className={styles.card}>
+      <ForgotPassword
+        onSubmit={values => {
+          alert(JSON.stringify(values))
+        }}
+      />
+    </Card>
   )
 }
 
