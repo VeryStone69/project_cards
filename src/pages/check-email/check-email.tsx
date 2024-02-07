c87cf7db4f864ec3e537134281d1c1b3ae8c3940
Alexander Khomenok <exleonardo@mail.ru>
2024-02-07 11:27:41 +0300

    feat:fix <Layout>

--- src/pages/check-email/check-email.tsx
+++ src/pages/check-email/check-email.tsx
@@ -1,4 +1,3 @@
-import { Layout } from '@/components/layout'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Typography } from '@/components/ui/typography'
@@ -9,20 +8,18 @@ import checkEmail from '../../assets/images/check-email.png'
 
 export const CheckEmail = () => {
   return (
-    <Layout>
-      <Card className={styles.card}>
-        <Typography className={styles.title} variant={'h1'}>
-          Check your email
-        </Typography>
+    <Card className={styles.card}>
+      <Typography className={styles.title} variant={'h1'}>
+        Check your email
+      </Typography>
 
-        <img alt={'check-email'} src={checkEmail} />
+      <img alt={'check-email'} src={checkEmail} />
 
-        <Typography className={styles.text} variant={'body2'}>
-          We’ve sent an Email with instructions to example@mail.com
-        </Typography>
+      <Typography className={styles.text} variant={'body2'}>
+        We’ve sent an Email with instructions to example@mail.com
+      </Typography>
 
-        <Button>Back to login</Button>
-      </Card>
-    </Layout>
+      <Button>Back to login</Button>
+    </Card>
   )
 }
