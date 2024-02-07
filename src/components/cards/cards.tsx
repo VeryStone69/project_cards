34bd774b45368685c16b46aac4e81ba688890139
Alexander Khomenok <exleonardo@mail.ru>
2024-02-07 11:29:22 +0300

    feat:fix <Layout>

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import { Layout } from '@/components/layout'
 import { BackButton } from '@/components/ui/back-button'
 import { Button } from '@/components/ui/button'
 import { Dropdown } from '@/components/ui/dropdown'
@@ -11,6 +10,7 @@ import { Table } from '@/components/ui/table'
 import { TableHeader } from '@/components/ui/table-header'
 import { TextField } from '@/components/ui/textField'
 import { Typography } from '@/components/ui/typography'
+import { DeleteCardButton } from '@/features/delete-card-button/delete-card-button'
 import { DropdownCard } from '@/features/dropdown-card/dropdown-card'
 import { useGetCardsInDeckQuery } from '@/services/cards-api/cards-api'
 import { useGetDeckInfoQuery } from '@/services/decks-api/decks-api'
@@ -24,6 +24,7 @@ export const Cards = () => {
   const [itemPerPage, setItemPerPage] = useState(5)
   const [currentPageDecks, setCurrentPage] = useState(1)
   const packId = id ?? ('' as string)
+  const isMyPack = 'authUserId === pack.author.id' || true
 
   const cards = useGetCardsInDeckQuery({
     packId,
@@ -54,74 +55,83 @@ export const Cards = () => {
   ]
 
   return (
-    <Layout>
-      <section style={{ marginTop: '100px' }}>
-        <BackButton />
-        <div style={{ marginTop: '34px' }}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Typography as={'h1'} variant={'large'}>
-              {pack.data?.name}
-            </Typography>
-            <Dropdown className={s.dropDown} sideOffset={-8}>
-              <DropdownCard />
-            </Dropdown>
-            <Button>Learn Card</Button>
-          </div>
-          {pack.data?.cover && (
-            <img
-              alt={'Cover'}
-              src={pack.data.cover}
-              style={{ height: '6rem', marginBottom: '24px', marginTop: '15px', width: '10rem' }}
-            />
-          )}
-
-          <TextField
-            clearField={() => {}}
-            onChange={() => {}}
-            placeholder={'Search by question'}
-            type={'search'}
-            value={''}
-          />
+    <section style={{ marginTop: '100px' }}>
+      <BackButton />
+      <div style={{ marginTop: '34px' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography as={'h1'} variant={'h1'}>
+            {pack.data?.name}
+          </Typography>
+          <Dropdown align={'center'} className={s.dropDown} sideOffset={-10}>
+            <DropdownCard />
+          </Dropdown>
+          <Button>Learn Card</Button>
         </div>
-        <Table.Root style={{ marginBottom: '24px', marginTop: '24px' }}>
-          <TableHeader columns={cardsColumns} />
-          <Table.Body>
-            {cards.data?.items.map(items => {
-              return (
-                <Table.Row key={items.id}>
-                  <Table.Cell align={'left'}>
-                    <img
-                      alt={'Pack cover'}
-                      src={items.questionImg ?? defaultMask}
-                      style={{ height: '48px', width: '110px' }}
-                    />
-                    {items.question}
-                  </Table.Cell>
-                  <Table.Cell>{items.answer}</Table.Cell>
-                  <Table.Cell> {new Date(items.updated).toLocaleDateString()}</Table.Cell>
-                  <Table.Cell>
-                    <Rating rating={items.grade} />
-                  </Table.Cell>
-                </Table.Row>
-              )
-            })}
-          </Table.Body>
-        </Table.Root>
-        <Pagination
-          className={s.pagination}
-          currentPage={currentPageDecks}
-          itemsPerPage={itemPerPage}
-          onChangeItemsPerPage={setItemPerPage}
-          onChangePage={setCurrentPage}
-          totalPages={cards.data?.pagination.totalItems}
+        {pack.data?.cover && (
+          <img
+            alt={'Cover'}
+            src={pack.data.cover}
+            style={{ height: '6rem', marginBottom: '24px', marginTop: '15px', width: '10rem' }}
+          />
+        )}
+
+        <TextField
+          clearField={() => {}}
+          onChange={() => {}}
+          placeholder={'Search by question'}
+          type={'search'}
+          value={''}
         />
-      </section>
-    </Layout>
+      </div>
+      <Table.Root style={{ marginBottom: '24px', marginTop: '24px' }}>
+        <TableHeader columns={cardsColumns} />
+        <Table.Body>
+          {cards.data?.items.map(items => {
+            return (
+              <Table.Row key={items.id}>
+                <Table.Cell align={'left'}>
+                  <img
+                    alt={'Pack cover'}
+                    src={items.questionImg ?? defaultMask}
+                    style={{ height: '48px', width: '110px' }}
+                  />
+                  {items.question}
+                </Table.Cell>
+                <Table.Cell>{items.answer}</Table.Cell>
+                <Table.Cell> {new Date(items.updated).toLocaleDateString()}</Table.Cell>
+                <Table.Cell>
+                  <Rating rating={items.grade} />
+                </Table.Cell>
+                <Table.Cell>
+                  <div>
+                    {isMyPack ? (
+                      <>
+                        <DeleteCardButton id={items.id} name={pack.data?.name || ''} />
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table.Root>
+      <Pagination
+        className={s.pagination}
+        currentPage={currentPageDecks}
+        itemsPerPage={itemPerPage}
+        onChangeItemsPerPage={setItemPerPage}
+        onChangePage={setCurrentPage}
+        totalPages={cards.data?.pagination.totalItems}
+      />
+    </section>
   )
 }
