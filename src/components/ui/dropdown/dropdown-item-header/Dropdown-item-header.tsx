bacc372964f9d0e2589a93890d6b24b6d61c06c0
Alexander Khomenok <exleonardo@mail.ru>
2024-02-07 11:30:21 +0300

    feat:add userLogo

--- src/components/ui/dropdown/dropdown-item-header/Dropdown-item-header.tsx
+++ src/components/ui/dropdown/dropdown-item-header/Dropdown-item-header.tsx
@@ -1,6 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react'
 
-import logo from '@/assets/images/avatar.jpg'
 import { Avatar } from '@/components/ui/avatar'
 import { Typography } from '@/components/ui/typography'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
@@ -11,6 +10,7 @@ export type DropDownHeaderProps = {
   className?: string
   onSelect?: (event: Event) => void
   userEmail?: string
+  userLogo: string
   userName: string
 } & ComponentPropsWithoutRef<'div'>
 export const DropdownItemHeader = ({
@@ -18,14 +18,15 @@ export const DropdownItemHeader = ({
   onSelect,
   style,
   userEmail,
+  userLogo,
   userName,
   ...props
 }: DropDownHeaderProps) => {
   return (
     <DropdownMenu.Item className={className} onSelect={onSelect} style={style} {...props}>
-      <Avatar className={s.avatar} src={logo} />
+      <Avatar className={s.avatar} src={userLogo} />
       <div>
-        <Typography variant={'subtitle1'}>{userName}</Typography>
+        <Typography variant={'subtitle2'}>{userName}</Typography>
         <Typography className={s.caption} variant={'caption'}>
           {userEmail}
         </Typography>
