890a5df7b3cdcef6918adaeee1133653572e4c2a
Alexander Khomenok <exleonardo@mail.ru>
2024-02-07 11:34:24 +0300

    feat: add logOut and fix avatar

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,38 +1,48 @@
 import { Icon } from '@/components/icon/Icon'
-import { Layout } from '@/components/layout'
+import { Avatar } from '@/components/ui/avatar'
+import { BackButton } from '@/components/ui/back-button'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { FileUploader } from '@/components/ui/file-uploader'
+import { IconButton } from '@/components/ui/icon-button'
 import { Typography } from '@/components/ui/typography'
+import { useLogOutMutation, useMeQuery } from '@/services/auth-api/auth'
 
 import s from './profile.module.scss'
 
-import avatar from '../../assets/images/avatar.jpg'
-
 export const Profile = () => {
+  const { data: me } = useMeQuery()
+  const [logOut] = useLogOutMutation()
+  const onlogOut = () => {
+    logOut()
+  }
+
   return (
-    <Layout>
+    <>
+      <BackButton className={s.backButton} />
       <Card className={s.card}>
         <Typography className={s.title} variant={'h1'}>
           Personal Information
         </Typography>
         <div className={s.avatarBlock}>
-          <img alt={'avatar'} className={s.avatar} src={avatar} />
+          <Avatar className={s.avatar} src={me?.avatar || ''} />
           <div className={s.fileUploader}>
             <FileUploader />
           </div>
         </div>
         <div className={s.nameBlock}>
-          <Typography variant={'h2'}>Artyom</Typography>
-          <Icon fill={'white'} height={'17px'} name={'edit'} width={'17px'} />
+          <Typography variant={'h2'}>{me?.name}</Typography>
+          <IconButton icon={<Icon name={'edit'} size={'17px'} />} />
         </div>
         <Typography className={s.email} variant={'body2'}>
-          example@gmail.com
+          {me?.email}
         </Typography>
         <Typography variant={'subtitle2'}>
-          <Button variant={'secondary'}>Logout</Button>
+          <Button onClick={onlogOut} variant={'secondary'}>
+            Logout
+          </Button>
         </Typography>
       </Card>
-    </Layout>
+    </>
   )
 }
