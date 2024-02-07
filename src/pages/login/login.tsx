fa76bd0d89b4e10a545b5c61616621f35aabd34e
Alexander Khomenok <exleonardo@mail.ru>
2024-02-07 11:19:07 +0300

    feat: add logout and add navigate

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,6 @@
+import { useNavigate } from 'react-router-dom'
+
 import { SignIn } from '@/components/forms/sign-in'
-import { Layout } from '@/components/layout'
 import { Card } from '@/components/ui/card'
 import { useLoginMutation } from '@/services/auth-api/auth'
 import { LoginData } from '@/services/auth-api/auth.types'
@@ -8,11 +9,11 @@ import styles from './login.module.scss'
 
 export const Login = () => {
   const [login] = useLoginMutation()
-
+  const navigate = useNavigate()
   const handleLogin = async (data: LoginData) => {
     try {
       await login(data).unwrap()
-
+      navigate('/')
       alert(`${data.email} is authorized!`)
     } catch (error: any) {
       // toast.error(error?.data?.message)
@@ -20,10 +21,8 @@ export const Login = () => {
   }
 
   return (
-    <Layout>
-      <Card className={styles.card}>
-        <SignIn onSubmit={handleLogin} />
-      </Card>
-    </Layout>
+    <Card className={styles.card}>
+      <SignIn onSubmit={handleLogin} />
+    </Card>
   )
 }
