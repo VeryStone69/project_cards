bd106083c677cd79159377df74d974d21c80b5cd
Alexander Khomenok <exleonardo@mail.ru>
2024-02-13 23:07:34 +0300

    feat: add ComponentPropsWithoutRef

--- src/features/button-footer/button-footer.tsx
+++ src/features/button-footer/button-footer.tsx
@@ -1,10 +1,12 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 import { clsx } from 'clsx'
 
 import s from './button-footer.module.scss'
 
-type Props = {
+type Props<T extends ElementType = 'div'> = {
   className?: string
   fullWidth?: boolean
   onClickCancel?: () => void
@@ -12,7 +14,7 @@ type Props = {
   option?: number
   titleCancel?: string
   titleConfirm?: string
-}
+} & ComponentPropsWithoutRef<T>
 export const ButtonFooter = ({
   className,
   fullWidth = false,
@@ -21,11 +23,12 @@ export const ButtonFooter = ({
   option = 1,
   titleCancel = 'Cancel',
   titleConfirm = 'Save',
+  ...rest
 }: Props) => {
   const classes = clsx(option == 1 && s.buttonSingle, option === 2 && s.buttonGroup, className)
 
   return (
-    <div className={classes}>
+    <div className={classes} {...rest}>
       {option === 2 && (
         <Button fullWidth={fullWidth} onClick={onClickCancel} variant={'secondary'}>
           <Typography variant={'subtitle2'}>{titleCancel}</Typography>
