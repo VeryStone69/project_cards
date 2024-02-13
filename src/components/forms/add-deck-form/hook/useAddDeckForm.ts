641591f84c89d2b307c0fe57cf943271a6222f84
Alexander Khomenok <exleonardo@mail.ru>
2024-02-13 20:26:41 +0300

    feat: job version final

--- src/components/forms/add-deck-form/hook/useAddDeckForm.ts
+++ src/components/forms/add-deck-form/hook/useAddDeckForm.ts
@@ -1,70 +1,44 @@
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
 
-import { addNewDeckSchema } from '@/utils/zod-resolvers/file-update-resolver'
+import { PackFormType, addNewDeckSchema } from '@/utils/zod-resolvers/file-update-resolver'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 export const useAddDeckForm = () => {
-  const [imgT, setImg] = useState<null | string>(null)
   const {
     control,
-    formState: { defaultValues, errors },
+    formState: { errors },
     getFieldState,
     getValues,
     handleSubmit,
-
+    reset,
     trigger,
     watch,
-  } = useForm({
+  } = useForm<PackFormType>({
     defaultValues: {
       cover: null,
       isPrivate: false,
       name: '',
     },
-    mode: 'onChange',
+
     resolver: zodResolver(addNewDeckSchema),
   })
 
-  // const file = getValues('cover')
+  const file = watch('cover')
 
-  // const file = watch('cover')
+  const img = file && !errors.cover ? URL.createObjectURL(file) : null
 
-  // const img = file && !errors.cover && dirtyFields.cover ? URL.createObjectURL(file) : null
-
-  // console.log(img)
   const deleteCover = async () => {
-    // reset({ ...getValues(), cover: null })
-    // watch('cover')
+    reset({ ...getValues(), cover: null })
     toast.warning('You deleted cover')
-    setImg('')
   }
   const toastError = async () => {
     const success = await trigger('cover')
     const { error } = getFieldState('cover')
-    const file = getValues('cover')
 
-    if (file) {
-      const badCase = defaultValues?.cover ?? null
-      const img = success ? URL.createObjectURL(file) : badCase
-
-      setImg(img)
-    }
-    // const img = file && !errors.cover && dirtyFields.cover ? URL.createObjectURL(file) : null
-    //
-    // if (success) {
-    //   setImg(img)
-    // }
-    // const file = watch('cover')
-
-    // if (success) {
-    //   setValue('cover', file)
-    // }
-    // resetField('cover')
     if (!success && error?.message) {
       toast.error(error.message)
-      setImg(null)
-      // resetField('cover')
+      reset({ ...getValues(), cover: null })
     }
   }
 
@@ -73,7 +47,7 @@ export const useAddDeckForm = () => {
     deleteCover,
     errors,
     handleSubmit,
-    imgT,
+    img,
     toastError,
   }
 }
