1575ee2bed0bf80dc94cffc690d59869781c51b4
Alexander Khomenok <exleonardo@mail.ru>
2024-02-13 23:15:35 +0300

    feat: add new func

--- src/components/forms/add-deck-form/add-deck-form.tsx
+++ src/components/forms/add-deck-form/add-deck-form.tsx
@@ -1,15 +1,15 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
-import { useForm } from 'react-hook-form'
 
 import notImg from '@/assets/images/not-img.jpg'
+import { useAddDeckForm } from '@/components/forms/add-deck-form/hook/useAddDeckForm'
 import { Icon } from '@/components/icon/Icon'
+import { Button } from '@/components/ui/button'
 import { ControlledCheckbox } from '@/components/ui/controlled/controlled-checkbox/controlled-checkbox'
 import { ControlledFileUploader } from '@/components/ui/controlled/controlled-file-uploader/controlled-file-uploader'
 import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field/controlled-text-field'
 import { Typography } from '@/components/ui/typography'
 import { ButtonFooter } from '@/features/button-footer'
-import { PackFormType, addNewDeckSchema } from '@/utils/zod-resolvers/file-update-resolver'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { PackFormType } from '@/utils/zod-resolvers/file-update-resolver'
 import { clsx } from 'clsx'
 
 import s from './add-deck-form.module.scss'
@@ -20,50 +20,44 @@ type Props<C extends ElementType = 'div'> = {
 } & Omit<ComponentPropsWithoutRef<C>, 'onSubmit'>
 export const AddDeckForm = ({ className, onCancel, onSubmit, ...rest }: Props) => {
   const classNames = clsx(s.formCard, className)
-
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    watch,
-  } = useForm({
-    defaultValues: {
-      cover: null,
-      isPrivate: false,
-      name: '',
-    },
-    mode: 'onChange',
-    resolver: zodResolver(addNewDeckSchema),
-  })
-
-  const file = watch('cover')
-  const src = file && !errors.cover ? URL.createObjectURL(file) : null
+  const { control, deleteCover, handleSubmit, img, open, setOpen, toastError } = useAddDeckForm()
+  const imgClasses = clsx(s.image, img && s.hover, open && s.open)
+  const onClickHandler = () => {
+    if (img) {
+      setOpen(prevState => !prevState)
+    }
+  }
 
   return (
     <div className={classNames} {...rest}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={s.inputBlock}>
-          <img alt={'notImg'} src={src ?? notImg} />
-          <Typography style={{ color: 'red' }} variant={'subtitle1'}>
-            {errors.cover?.message}
-          </Typography>
-          <ControlledFileUploader
-            control={control}
-            name={'cover'}
-            type={'button'}
-            variant={'secondary'}
-          >
-            <Icon className={s.imgOnButton} name={'img'} viewBox={'0 0 18 18'} />
-            <Typography variant={'subtitle2'}>Upload Image</Typography>
-          </ControlledFileUploader>
-
+          <img alt={'notImg'} className={imgClasses} onClick={onClickHandler} src={img || notImg} />
+          <div className={s.deleteCover}>
+            {img && (
+              <Button fullWidth onClick={deleteCover} type={'reset'} variant={'secondary'}>
+                <Typography variant={'subtitle2'}>Delete Cover</Typography>
+                <Icon fill={'white'} name={'trashBin'} size={'18px'} />
+              </Button>
+            )}
+            <ControlledFileUploader
+              control={control}
+              errorToast={toastError}
+              fullWidth
+              name={'cover'}
+              type={'button'}
+              variant={'secondary'}
+            >
+              <Icon className={s.imgOnButton} name={'img'} viewBox={'0 0 18 18'} />
+              <Typography variant={'subtitle2'}>Upload Image</Typography>
+            </ControlledFileUploader>
+          </div>
           <ControlledTextField control={control} label={'Deck name'} name={'name'} />
 
           <Typography variant={'body2'}>
             <ControlledCheckbox control={control} label={'Private deck'} name={'isPrivate'} />
           </Typography>
         </div>
-
         <ButtonFooter onClickCancel={onCancel} option={2} titleConfirm={'Create a deck'} />
       </form>
     </div>
