0df74b5806a2c54784a489ba639790be7434d838
atryomkorshykau <butovichjunior@gmail.com>
2024-02-14 17:23:47 +0300

    fix: img in table

--- src/widgets/table-content-deck/table-content-deck.tsx
+++ src/widgets/table-content-deck/table-content-deck.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-import defaultMask from '@/assets/images/Mask.jpg'
+import defaultMask from '@/assets/images/not-img.jpg'
 import { Button } from '@/components/ui/button'
 import { Table } from '@/components/ui/table'
 import { Typography } from '@/components/ui/typography'
