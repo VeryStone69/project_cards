52bf4237223ad42491a8d01567d8e361424f6b49
Alexander Khomenok <exleonardo@mail.ru>
2024-02-15 23:12:49 +0300

    feat: move onSubmit to CreateAndModifyDeckForm

--- src/features/add-new-deck/add-new-deck.tsx
+++ src/features/add-new-deck/add-new-deck.tsx
@@ -1,36 +1,24 @@
 import { useState } from 'react'
-import { SubmitHandler } from 'react-hook-form'
 
-import { AddDeckForm } from '@/components/forms/add-deck-form'
+import { CreateAndModifyDeckForm } from '@/components/forms/add-deck-form'
 import { Button } from '@/components/ui/button'
 import { Modal } from '@/components/ui/modal'
 import { Typography } from '@/components/ui/typography'
 import { useCreateDecksMutation } from '@/services/decks-api/decks-api'
-import { PackFormType } from '@/utils/zod-resolvers/file-update-resolver'
 
 export const AddNewDeck = () => {
   const [open, setOpen] = useState(false)
   const [createDeck] = useCreateDecksMutation()
-  const onSubmit: SubmitHandler<PackFormType> = async data => {
-    const form = new FormData()
-
-    form.append('name', data.name)
-    form.append('isPrivate', `${data.isPrivate}`)
-
-    if (data.cover === null) {
-      form.append('cover', '')
-    } else {
-      form.append('cover', data.cover || '')
-    }
+  const addDeckHandler = async (data: FormData) => {
     setOpen(!open)
-    await createDeck(form).unwrap()
+    await createDeck(data).unwrap()
   }
 
   return (
     <div>
       {open && (
         <Modal open={open} setOpen={setOpen} title={'Creating a new deck'}>
-          <AddDeckForm onCancel={() => setOpen(false)} onSubmit={onSubmit} />
+          <CreateAndModifyDeckForm onCancel={() => setOpen(false)} onSubmit={addDeckHandler} />
         </Modal>
       )}
 
