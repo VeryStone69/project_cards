7bbed09de92d24aebcd681b535318f6d93bd9c2b
Alexander Khomenok <exleonardo@mail.ru>
2024-02-16 22:58:10 +0300

    feat: add new component

---
+++ src/features/profile-name-update/profile-name-update.tsx
@@ -0,0 +1,63 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { SubmitHandler } from 'react-hook-form'
+import { toast } from 'react-toastify'
+
+import { EditProfile } from '@/components/forms/personal-information'
+import { Icon } from '@/components/icon/Icon'
+import { Button } from '@/components/ui/button'
+import { IconButton } from '@/components/ui/icon-button'
+import { Typography } from '@/components/ui/typography'
+import { useLogOutMutation, useUpdateProfileMutation } from '@/services/auth-api/auth'
+
+import s from '@/pages/profile/profile.module.scss'
+
+type Props<T extends ElementType = 'div'> = {
+  as?: T
+  email: string
+  isEdit: boolean
+  name: string
+  setEdit: (value: boolean) => void
+} & Omit<ComponentPropsWithoutRef<T>, 'name'>
+export const ProfileNameUpdate = <T extends ElementType = 'div'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props>
+) => {
+  const { as, email, isEdit, name, setEdit, ...rest } = props
+  const Component = as || 'div'
+  const [logOut] = useLogOutMutation()
+  const [updateProfile] = useUpdateProfileMutation()
+  const onLogOut = () => {
+    logOut()
+  }
+
+  const onSubmitHandler: SubmitHandler<{ name: string }> = async data => {
+    const form = new FormData()
+
+    form.append('name', data.name)
+
+    setEdit(false)
+    await updateProfile(form).unwrap()
+    toast.success('name successful changed')
+  }
+
+  return (
+    <>
+      {!isEdit ? (
+        <>
+          <Component {...rest} className={s.nameBlock}>
+            <Typography variant={'h2'}>{name}</Typography>
+            <IconButton icon={<Icon name={'edit'} size={'17px'} />} onClick={() => setEdit(true)} />
+          </Component>
+          <Typography className={s.email} variant={'body2'}>
+            {email}
+          </Typography>
+          <Button onClick={onLogOut} variant={'secondary'}>
+            <Icon fill={'white'} name={'logout'} size={'16px'} />
+            <Typography variant={'subtitle2'}>Logout</Typography>
+          </Button>
+        </>
+      ) : (
+        <EditProfile name={name} onSubmit={onSubmitHandler} />
+      )}
+    </>
+  )
+}
