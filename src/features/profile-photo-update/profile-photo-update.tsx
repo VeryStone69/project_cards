10a49fe50a125c068031e3697c08fbab1e97c43d
Alexander Khomenok <exleonardo@mail.ru>
2024-02-17 22:47:43 +0300

    feat: add profile-photo-update.tsx

---
+++ src/features/profile-photo-update/profile-photo-update.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import { ProfilePhotoLoader } from '@/components/forms/profile-photo-loader/profile-photo-loader'
+import { Avatar } from '@/components/ui/avatar'
+
+import s from '@/pages/profile/profile.module.scss'
+
+type Props<T extends ElementType = 'div'> = {
+  as?: T
+  avatar: string | undefined
+  isEdit: boolean
+} & Omit<ComponentPropsWithoutRef<T>, any>
+export const ProfilePhotoUpdate = <T extends ElementType = 'div'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props>
+) => {
+  const { as, avatar, isEdit } = props
+  const Component = as || 'div'
+
+  return (
+    <Component className={s.avatarBlock}>
+      <Avatar className={s.avatar} src={avatar} />
+      {!isEdit && <ProfilePhotoLoader />}
+    </Component>
+  )
+}
