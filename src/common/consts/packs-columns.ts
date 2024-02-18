89cbc08152334a360f7fb93bab16e0be91477ad0
Alexander Khomenok <exleonardo@mail.ru>
2024-02-18 12:16:02 +0300

    feat: add packs-columns.ts

---
+++ src/common/consts/packs-columns.ts
@@ -0,0 +1,27 @@
+export const packsColumns = [
+  {
+    key: 'name',
+    sortable: true,
+    title: 'Name',
+  },
+  {
+    key: 'cardsCount',
+    sortable: true,
+    title: 'Cards',
+  },
+  {
+    key: 'updated',
+    sortable: true,
+    title: 'Last Updated',
+  },
+  {
+    key: 'created',
+    sortable: true,
+    title: 'Created By',
+  },
+  {
+    key: 'controls',
+    sortable: true,
+    title: '',
+  },
+]
