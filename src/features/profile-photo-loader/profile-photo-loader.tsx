8be9f93f3fc3f58ad9f58b11e48df34769024232
Alexander Khomenok <exleonardo@mail.ru>
2024-02-18 12:49:23 +0300

    feat: add custom hook

--- src/features/profile-photo-loader/profile-photo-loader.tsx
+++ src/features/profile-photo-loader/profile-photo-loader.tsx
@@ -1,54 +1,15 @@
-import { ChangeEvent } from 'react'
-import { toast } from 'react-toastify'
-
 import { Icon } from '@/components/icon/Icon'
 import { FileUploader } from '@/components/ui/file-uploader'
 import { IconButton } from '@/components/ui/icon-button'
-import { useUpdateProfileMutation } from '@/services/auth-api/auth'
-import { coverSchema } from '@/utils/zod-resolvers/file-update-resolver'
-import * as z from 'zod'
+import { useProfilePhotoLoader } from '@/features/profile-photo-loader/hook/use-profile-photo-loader'
 
 import s from './profile-photo-loader.module.scss'
+
 type Props = {
   avatar: string | undefined
 }
 export const ProfilePhotoLoader = ({ avatar }: Props) => {
-  const [updateProfile] = useUpdateProfileMutation()
-
-  const onChangeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
-    try {
-      const file = e.target?.files?.[0]
-
-      coverSchema.parse(file)
-      const form = new FormData()
-
-      form.append('avatar', file || '')
-
-      await toast.promise(updateProfile(form).unwrap(), {
-        pending: 'avatar is updating',
-        success: 'avatar successful changed',
-      })
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        toast.error(err.issues[0].message)
-      } else {
-        toast.error('avatar not upload')
-      }
-    }
-  }
-  const deleteAvatar = async () => {
-    const form = new FormData()
-
-    form.append('avatar', '')
-    try {
-      await toast.promise(updateProfile(form).unwrap(), {
-        pending: 'avatar is deleting',
-        success: 'avatar successful deleted',
-      })
-    } catch (err) {
-      toast.error('avatar not deleted')
-    }
-  }
+  const { deleteAvatar, onChangeHandler } = useProfilePhotoLoader()
 
   return (
     <div className={s.fileUploader}>
