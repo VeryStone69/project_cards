3d209bbc42508370b781cebdf1de28678d96a36e
Alexander Khomenok <exleonardo@mail.ru>
2024-02-20 23:34:43 +0300

    refactor: fix isAuthenticated

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
 import { Header } from '@/components/header'
-import { InitialLoader } from '@/components/ui/loader'
 import { useMeQuery } from '@/services/auth-api/auth'
 
 import s from './layout.module.scss'
@@ -11,19 +10,15 @@ type Props = {
 }
 export const Layout = ({ className }: Props) => {
   const { data, isError, isLoading } = useMeQuery()
-  const isAuthenticated = !isError && !isLoading
-
-  if (isLoading) {
-    return <InitialLoader />
-  }
+  const isAuthenticated = (!isError && !isLoading && data !== null) || false
 
   return (
     <>
       <Header
-        avatar={data?.avatar || ''}
-        email={data?.email || ''}
+        avatar={data?.avatar || undefined}
+        email={data?.email}
         isAuthenticated={isAuthenticated}
-        userName={data?.name || ''}
+        userName={data?.name}
       />
       <div className={className ? className : s.container}>
         <Outlet />
