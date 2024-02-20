acc2508cd9b3a6a974aa2e6751a1211c1af1dede
Alexander Khomenok <exleonardo@mail.ru>
2024-02-20 23:38:15 +0300

    refactor: link and signUpSchema

--- src/components/forms/sign-up/sign-up.tsx
+++ src/components/forms/sign-up/sign-up.tsx
@@ -1,33 +1,16 @@
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field/controlled-text-field'
 import { Typography } from '@/components/ui/typography'
-import { DevTool } from '@hookform/devtools'
+import { RegisterForm, signUpSchema } from '@/utils/zod-resolvers/file-update-resolver'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { clsx } from 'clsx'
-import { z } from 'zod'
 
 import s from './sign-up.module.scss'
 
 import { Button } from '../../ui/button'
 
-const emailRegex =
-  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
-
-const SignUpSchema = z.object({
-  confirmPassword: z
-    .string()
-    .min(3, 'Пароль должен быть не менее 3 символов')
-    .max(30, 'Пароль должен быть не более 30 символов'),
-  email: z.string().trim().email('Введите действующий адрес электронной почты').regex(emailRegex),
-  password: z
-    .string()
-    .min(3, 'Пароль должен быть не менее 3 символов')
-    .max(30, 'Пароль должен быть не более 30 символов'),
-  rememberMe: z.boolean().optional(),
-})
-
-export type RegisterForm = z.infer<typeof SignUpSchema>
 type LoginProps = {
   className?: string
   onSubmit: (values: RegisterForm) => void
@@ -39,17 +22,14 @@ export const SignUp = ({ className, onSubmit }: LoginProps) => {
       confirmPassword: '',
       email: '',
       password: '',
-      rememberMe: false,
     },
-    resolver: zodResolver(SignUpSchema),
+    resolver: zodResolver(signUpSchema),
   })
 
   const classNames = clsx(s.form, className)
 
   return (
     <div className={s.formCard}>
-      <DevTool control={control} />
-
       <Typography className={s.title} variant={'h1'}>
         Registration
       </Typography>
@@ -82,7 +62,7 @@ export const SignUp = ({ className, onSubmit }: LoginProps) => {
         <Typography variant={'body2'}>Already have an account?</Typography>
 
         <Typography className={s.signinButton} variant={'h2'}>
-          <a href={'#'}>Login</a>
+          <Link to={'/login'}>Login</Link>
         </Typography>
       </div>
     </div>
