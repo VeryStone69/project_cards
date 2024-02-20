79b76b312f3fa03bc7728179274a1bfff76f59c7
Alexander Khomenok <exleonardo@mail.ru>
2024-02-20 23:32:24 +0300

    feat: change name style

--- src/pages/recovery-password/recovery-password.tsx
+++ src/pages/recovery-password/recovery-password.tsx
@@ -1,11 +1,11 @@
 import { ForgotPassword } from '@/components/forms/forgot-password'
 import { Card } from '@/components/ui/card'
 
-import styles from './recovery-password.module.scss'
+import s from './recovery-password.module.scss'
 
 const RecoveryPassword = () => {
   return (
-    <Card className={styles.card}>
+    <Card className={s.card}>
       <ForgotPassword
         onSubmit={values => {
           alert(JSON.stringify(values))
