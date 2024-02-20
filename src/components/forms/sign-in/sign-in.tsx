6de6e84cf2a8193b26e4a246c6a905ffb6f3b107
Alexander Khomenok <exleonardo@mail.ru>
2024-02-20 23:41:16 +0300

    refactor: fix link and SignInSchema

--- src/components/forms/sign-in/sign-in.tsx
+++ src/components/forms/sign-in/sign-in.tsx
@@ -1,30 +1,17 @@
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 import { ControlledCheckbox } from '@/components/ui/controlled/controlled-checkbox/controlled-checkbox'
 import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field/controlled-text-field'
 import { Typography } from '@/components/ui/typography'
-import { DevTool } from '@hookform/devtools'
+import { FormValues, SignInSchema } from '@/utils/zod-resolvers/file-update-resolver'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { clsx } from 'clsx'
-import { z } from 'zod'
 
 import s from './sign-in.module.scss'
 
 import { Button } from '../../ui/button'
 
-const emailRegex =
-  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
-
-const SignInSchema = z.object({
-  email: z.string().trim().email('Введите действующий адрес электронной почты').regex(emailRegex),
-  password: z
-    .string()
-    .min(3, 'Пароль должен быть не менее 3 символов')
-    .max(30, 'Пароль должен быть не более 30 символов'),
-  rememberMe: z.boolean().optional(),
-})
-
-export type FormValues = z.infer<typeof SignInSchema>
 type LoginProps = {
   className?: string
   onSubmit: (values: FormValues) => void
@@ -44,8 +31,6 @@ export const SignIn = ({ className, onSubmit }: LoginProps) => {
 
   return (
     <div className={s.formCard}>
-      <DevTool control={control} />
-
       <Typography className={s.title} variant={'h1'}>
         Login to account
       </Typography>
@@ -69,10 +54,12 @@ export const SignIn = ({ className, onSubmit }: LoginProps) => {
         />
 
         <Typography className={s.forgot} variant={'body2'}>
-          <a href={'#'}>forgot password?</a>
+          <Link className={s.forgotLink} to={'/recovery'}>
+            forgot password?
+          </Link>
         </Typography>
 
-        <Button className={s.button} fullWidth type={'submit'}>
+        <Button className={s.button} fullWidth>
           <Typography variant={'subtitle2'}>Login</Typography>
         </Button>
       </form>
@@ -80,8 +67,10 @@ export const SignIn = ({ className, onSubmit }: LoginProps) => {
       <div className={s.signup}>
         <Typography variant={'body2'}>Don`t have an account?</Typography>
 
-        <Typography className={s.signupButton} variant={'h2'}>
-          <a href={'#'}>Register</a>
+        <Typography className={s.register} variant={'h2'}>
+          <Link className={s.registerLink} to={'/register'}>
+            Register
+          </Link>
         </Typography>
       </div>
     </div>
