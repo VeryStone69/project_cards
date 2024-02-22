a31556845a89a72d86ef44e578e4bad9f5c6c6b4
Alexander Khomenok <exleonardo@mail.ru>
2024-02-22 21:03:46 +0300

    feat: add submitHandler CreatePassword

--- src/pages/create-new-password/create-new-password.tsx
+++ src/pages/create-new-password/create-new-password.tsx
@@ -1,16 +1,36 @@
+import { SubmitHandler } from 'react-hook-form'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 import { CreatePassword } from '@/components/forms/create-password'
 import { Card } from '@/components/ui/card'
+import { useResetPasswordMutation } from '@/services/auth-api/auth'
+import { errorNotification } from '@/utils/error-notification/error-notification'
+import { CreateNewPasswordForm } from '@/utils/zod-resolvers/file-update-resolver'
 
 import styles from './create-new-password.module.scss'
 
 export const CreateNewPassword = () => {
+  const navigate = useNavigate()
+  const { token } = useParams<{ token: string }>()
+  const [resetPassword] = useResetPasswordMutation()
+  const submitHandler: SubmitHandler<CreateNewPasswordForm> = async data => {
+    try {
+      if (token) {
+        await toast.promise(resetPassword({ data, token }).unwrap(), {
+          pending: 'password recovery',
+          success: 'password changed successfully',
+        })
+        navigate(`/login`)
+      }
+    } catch (err) {
+      errorNotification(err)
+    }
+  }
+
   return (
     <Card className={styles.card}>
-      <CreatePassword
-        onSubmit={values => {
-          alert(JSON.stringify(values))
-        }}
-      />
+      <CreatePassword onSubmit={submitHandler} />
     </Card>
   )
 }
