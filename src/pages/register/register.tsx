427b80c962dbc8bfc38bf7287184846b199872c2
Alexander Khomenok <exleonardo@mail.ru>
2024-02-22 20:56:39 +0300

    feat: add register

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,19 +1,33 @@
+import { SubmitHandler } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 import { SignUp } from '@/components/forms/sign-up'
 import { Card } from '@/components/ui/card'
+import { PATH } from '@/router'
 import { useRegisterMutation } from '@/services/auth-api/auth'
+import { errorNotification } from '@/utils/error-notification/error-notification'
 import { RegisterForm } from '@/utils/zod-resolvers/file-update-resolver'
 
-import styles from './register.module.scss'
+import s from './register.module.scss'
 
 export const Register = () => {
   const [register] = useRegisterMutation()
-
-  const handleRegister = async ({ confirmPassword, ...rest }: RegisterForm) => {
-    await register(rest).unwrap()
+  const navigate = useNavigate()
+  const handleRegister: SubmitHandler<RegisterForm> = async ({ confirmPassword, ...rest }) => {
+    try {
+      await toast.promise(register(rest).unwrap(), {
+        pending: 'register user',
+        success: `You are successful register!`,
+      })
+      navigate(`${PATH.login}`)
+    } catch (err) {
+      errorNotification(err)
+    }
   }
 
   return (
-    <Card className={styles.card}>
+    <Card className={s.card}>
       <SignUp onSubmit={handleRegister} />
     </Card>
   )
