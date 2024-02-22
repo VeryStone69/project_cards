5c999762e1db13097e3d34c9c972b33ea89d059c
Alexander Khomenok <exleonardo@mail.ru>
2024-02-22 21:33:25 +0300

    feat: refactor Router

---
+++ src/common/consts/routes.ts
@@ -0,0 +1,11 @@
+export const PATH = {
+  check: '/check-email',
+  createNewPassword: '/create-new-password',
+  decks: '/',
+  home: '/home',
+  login: '/login',
+  notFound: '/page-not-found',
+  profile: '/profile',
+  recover: '/recovery',
+  register: '/register',
+}
