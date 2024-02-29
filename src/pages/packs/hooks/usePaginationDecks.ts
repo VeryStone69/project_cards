bbd6ddf2bfb1bf86b88a96ae3395d83306cca8d8
Alexander Khomenok <exleonardo@mail.ru>
2024-02-29 21:47:38 +0300

    feat: add uri params

--- src/pages/packs/hooks/usePaginationDecks.ts
+++ src/pages/packs/hooks/usePaginationDecks.ts
@@ -1,18 +1,25 @@
 import { useCallback } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@/app/hooks'
-import { currentPageSelector, pageSizeSelector } from '@/pages/packs/selectors/packs-selectors'
+import { useAppDispatch } from '@/app/hooks'
 import { packsActions } from '@/store/packs-slice/packs-slice'
 
-export const usePaginationDecks = () => {
+export const usePaginationDecks = (
+  searchParams: URLSearchParams,
+  setSearchParams: (searchParams: URLSearchParams) => void
+) => {
   const dispatch = useAppDispatch()
-  const currentPage = useAppSelector(currentPageSelector)
-  const itemsPerPage = useAppSelector(pageSizeSelector)
+
+  const currentPage = Number(searchParams.get('currentPage')) || 1
+  const itemsPerPage = Number(searchParams.get('itemsPerPage')) || 5
   const changePage = useCallback((page: number) => {
-    dispatch(packsActions.setCurrentPage({ newPage: page }))
+    searchParams.set('currentPage', String(page))
+    setSearchParams(searchParams)
+    dispatch(packsActions.setCurrentPage({ newPage: Number(searchParams.get('currentPage')) }))
   }, [])
   const changeItemPerPage = useCallback((item: number) => {
-    dispatch(packsActions.setPageSize({ newPageSize: item }))
+    searchParams.set('itemsPerPage', String(item))
+    setSearchParams(searchParams)
+    dispatch(packsActions.setPageSize({ newPageSize: Number(searchParams.get('itemsPerPage')) }))
   }, [])
 
   return {
