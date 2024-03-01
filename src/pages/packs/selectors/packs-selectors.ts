d684209cec6c0da5345fb41a31bf720f0c18cdd4
Alexander Khomenok <exleonardo@mail.ru>
2024-03-01 16:56:16 +0300

    feat: change return type

--- src/pages/packs/selectors/packs-selectors.ts
+++ src/pages/packs/selectors/packs-selectors.ts
@@ -1,9 +1,8 @@
 import { RootState } from '@/services/store'
 
-export const pageSizeSelector = (state: RootState): number => state.packs.pagination.pageSize
-export const searchNameSelector = (state: RootState): string => state.packs.filter.searchName
-export const tabValueSelector = (state: RootState): string => state.packs.filter.tabValue
-export const sliderValueMinSelector = (state: RootState): number =>
-  state.packs.filter.sliderValueMin
+export const pageSizeSelector = (state: RootState) => state.packs.pagination.pageSize
+export const searchNameSelector = (state: RootState) => state.packs.filter.searchName
+export const tabValueSelector = (state: RootState) => state.packs.filter.tabValue
+export const sliderValueMinSelector = (state: RootState) => state.packs.filter.sliderValueMin
 export const sliderValueMaxSelector = (state: RootState) => state.packs.filter.sliderValueMax
-export const currentPageSelector = (state: RootState): number => state.packs.pagination.currentPage
+export const currentPageSelector = (state: RootState) => state.packs.pagination.currentPage
