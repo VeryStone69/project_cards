c34192ce7be741fbd8d208f2a5901a1ffe8000f5
Alexander Khomenok <exleonardo@mail.ru>
2024-03-01 16:40:22 +0300

    feat: change type

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -7,19 +7,17 @@ import s from './avatar.module.scss'
 type Props = {
   className?: string
   src?: string
-  userName?: string
+  userName: string
 }
-export const Avatar = forwardRef<any, Props>(
-  ({ className, src, userName = 'test', ...props }, ref) => {
-    const userNames = userName?.length > 4 ? userName[0] : userName
+export const Avatar = forwardRef<any, Props>(({ className, src, userName = '', ...props }, ref) => {
+  const userNames = userName.length > 4 ? userName[0] : userName
 
-    return (
-      <AvatarRadix.Root className={className ? className : s.avatarRoot} ref={ref} {...props}>
-        <AvatarRadix.Image alt={'User Avatar'} className={s.image} src={src} />
-        {!src && (
-          <AvatarRadix.Fallback className={s.avatarFallback}>{userNames}</AvatarRadix.Fallback>
-        )}
-      </AvatarRadix.Root>
-    )
-  }
-)
+  return (
+    <AvatarRadix.Root className={className ? className : s.avatarRoot} ref={ref} {...props}>
+      <AvatarRadix.Image alt={'User Avatar'} className={s.image} src={src} />
+      {!src && (
+        <AvatarRadix.Fallback className={s.avatarFallback}>{userNames}</AvatarRadix.Fallback>
+      )}
+    </AvatarRadix.Root>
+  )
+})
