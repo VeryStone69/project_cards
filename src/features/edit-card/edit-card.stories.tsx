8721db9e4c39a88516a551513ae5df988754caea
atryomkorshykau <butovichjunior@gmail.com>
2024-03-01 19:18:14 +0300

    fix: edit card stories

--- src/features/edit-card/edit-card.stories.tsx
+++ src/features/edit-card/edit-card.stories.tsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'
 
+import { CreateAndModifyCardForm } from '@/components/forms/create-and-modify-card-form'
 import { Icon } from '@/components/icon/Icon'
-import { Button } from '@/components/ui/button'
-import { FileUploader } from '@/components/ui/file-uploader'
+import { IconButton } from '@/components/ui/icon-button'
 import { Modal } from '@/components/ui/modal'
-import { TextField } from '@/components/ui/textField'
-import { Typography } from '@/components/ui/typography'
-import { ButtonFooter } from '@/features/button-footer'
 import { Meta, StoryObj } from '@storybook/react'
-
-import s from 'src/features/add-new-card/add-and-update-card.scss'
-
-import notImg from '../../assets/images/not-img.jpg'
 
 const meta = {
   args: {
@@ -23,112 +16,46 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
-  title: 'Components/features/add-new-card',
+  title: 'Components/features/edit-card',
 } satisfies Meta<typeof Modal>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const AddCardModal: Story = {
+export const EditCard: Story = {
   render: () => {
     const [open, setOpen] = useState(false)
-    const [imgQestion, setImgQestion] = useState<File | null>(null)
-    const [imgAnswer, setImgAnswer] = useState<File | null>(null)
-    const imgQ = imgQestion ? URL.createObjectURL(imgQestion) : null
-    const imgA = imgAnswer ? URL.createObjectURL(imgAnswer) : null
+    const [option, setOption] = useState('1')
+    const options = [
+      {
+        title: 'Text',
+        value: '1',
+      },
+      {
+        title: 'Picture',
+        value: '2',
+      },
+    ]
 
     return (
-      <>
+      <div>
         {open && (
-          <Modal open={open} setOpen={setOpen} title={'Adding a new card'}>
-            <div className={s.inputBlock}>
-              <Typography variant={'subtitle1'}>
-                Enter your question and give the correct answer:
-              </Typography>
-              <TextField label={'Question:'} />
-              <img
-                alt={'notImg'}
-                src={imgQ ?? notImg}
-                style={{ objectFit: 'cover', width: '100%' }}
-              />
-              <div
-                style={{
-                  alignItems: 'center',
-                  display: 'flex',
-                  gap: '1rem',
-                  justifyContent: 'space-between',
-                }}
-              >
-                {imgQ && (
-                  <Button
-                    fullWidth
-                    onClick={() => setImgQestion(null)}
-                    type={'reset'}
-                    variant={'secondary'}
-                  >
-                    <Typography variant={'subtitle2'}>Delete Cover</Typography>
-                    <Icon fill={'white'} name={'trashBin'} size={'18px'} />
-                  </Button>
-                )}
-
-                <FileUploader
-                  name={''}
-                  onChange={e => {
-                    setImgQestion(e.target.files?.[0] || null)
-                    e.target.value = ''
-                  }}
-                  style={{ width: '100%' }}
-                  variant={'secondary'}
-                >
-                  <Icon className={s.imgOnButton} name={'img'} viewBox={'0 0 18 18'} />
-                  <Typography variant={'subtitle2'}>Change Image</Typography>
-                </FileUploader>
-              </div>
-
-              <TextField label={'Answer:'} />
-              <img alt={'notImg'} src={imgA ?? notImg} />
-              <div
-                style={{
-                  alignItems: 'center',
-                  display: 'flex',
-                  gap: '1rem',
-                  justifyContent: 'space-between',
-                }}
-              >
-                {imgA && (
-                  <Button
-                    fullWidth
-                    onClick={() => setImgAnswer(null)}
-                    type={'reset'}
-                    variant={'secondary'}
-                  >
-                    <Typography variant={'subtitle2'}>Delete Cover</Typography>
-                    <Icon fill={'white'} name={'trashBin'} size={'18px'} />
-                  </Button>
-                )}
-
-                <FileUploader
-                  name={''}
-                  onChange={e => {
-                    setImgAnswer(e.target.files?.[0] || null)
-                    e.target.value = ''
-                  }}
-                  style={{ width: '100%' }}
-                  variant={'secondary'}
-                >
-                  <Icon className={s.imgOnButton} name={'img'} viewBox={'0 0 18 18'} />
-                  <Typography variant={'subtitle2'}>Change Image</Typography>
-                </FileUploader>
-              </div>
-
-              <ButtonFooter onClickCancel={() => setOpen(false)} option={2} />
-            </div>
+          <Modal open={open} setOpen={setOpen} title={'Editing a card'}>
+            <CreateAndModifyCardForm
+              defaultValue={{}}
+              onCancel={() => setOpen(false)}
+              onSubmit={() => {}}
+              onValueChange={setOption}
+              options={options}
+              selectOption={option}
+            />
           </Modal>
         )}
-        <Button onClick={() => setOpen(true)} variant={'secondary'}>
-          Add Card
-        </Button>
-      </>
+
+        <IconButton
+          icon={<Icon height={'16px'} name={'edit'} onClick={() => setOpen(true)} width={'16px'} />}
+        />
+      </div>
     )
   },
 }
