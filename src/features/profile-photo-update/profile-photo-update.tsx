c24bb4a5cd9f37a6f4f1e00c59ea8262c4572eee
Alexander Khomenok <exleonardo@mail.ru>
2024-03-01 16:38:24 +0300

    feat: fix userName

--- src/features/profile-photo-update/profile-photo-update.tsx
+++ src/features/profile-photo-update/profile-photo-update.tsx
@@ -10,16 +10,17 @@ type Props<T extends ElementType = 'div'> = {
   as?: T
   avatar: string | undefined
   isEdit: boolean
+  userName?: string
 } & Omit<ComponentPropsWithoutRef<T>, any>
 export const ProfilePhotoUpdate = <T extends ElementType = 'div'>(
   props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props>
 ) => {
-  const { as, avatar, isEdit } = props
+  const { as, avatar, isEdit, userName = '' } = props
   const Component = as || 'div'
 
   return (
     <Component className={s.avatarBlock}>
-      <Avatar className={s.avatar} src={avatar} />
+      <Avatar className={s.avatar} src={avatar} userName={userName} />
       {!isEdit && <ProfilePhotoLoader avatar={avatar} />}
     </Component>
   )
