ed11c1219c25545ae4576c90f2a95e54f6765098
Alexander Khomenok <exleonardo@mail.ru>
2024-03-02 15:23:33 +0300

    feat: fix sort

--- src/common/consts/routes.ts
+++ src/common/consts/routes.ts
@@ -3,10 +3,10 @@ export const PATH = {
   check: '/check-email',
   createNewPassword: '/create-new-password',
   decks: '/decks',
+  learn: '/learn',
   login: '/login',
   notFound: '/*',
   profile: '/profile',
   recover: '/recovery',
   register: '/register',
-  learn: '/learn',
 } as const
