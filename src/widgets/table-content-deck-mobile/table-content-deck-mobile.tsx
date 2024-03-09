d15fd3a27fb08300c8617a4528e56e20ebb8c736
Alexander Khomenok <exleonardo@mail.ru>
2024-03-09 13:27:23 +0300

    feat: add no-content

--- src/widgets/table-content-deck-mobile/table-content-deck-mobile.tsx
+++ src/widgets/table-content-deck-mobile/table-content-deck-mobile.tsx
@@ -1,4 +1,5 @@
 import { Table } from '@/components/ui/table'
+import { Typography } from '@/components/ui/typography'
 import { DecksItems } from '@/services/decks-api/decks-api.types'
 import { TableContentMobile } from '@/widgets/table-content-deck-mobile/table-content-mobile/table-content-mobile'
 
@@ -9,6 +10,14 @@ type Props = {
   items?: DecksItems[]
 }
 export const TableContentDeckMobile = ({ currentUserId, items }: Props) => {
+  if (!items?.length) {
+    return (
+      <div className={s.noItems}>
+        <Typography variant={'body1'}>No content with these terms...</Typography>
+      </div>
+    )
+  }
+
   return (
     <Table.Root className={s.table}>
       <Table.Body>
